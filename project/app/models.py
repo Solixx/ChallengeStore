8ca21adb04890d1df993413e256c66648908358c
Manuel <manuel_jose_2002@hotmail.com>
2024-09-22 17:56:33 +0100

    Categorias & CRUD para categorias e produtos com soft-delete e restore

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,53 @@
 from django.db import models
+from django import forms
+
+# Managers
+
+class ActiveClassManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False) # This will return only the class content that are not deleted
+    
+class AllClassManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().all() # This will return all the class content
+    
+class DeletedClassManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=True) # This will return only the class content  that are deleted
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+
+    # Soft delete field
+    is_deleted = models.BooleanField(default=False)
+
+    objects = AllClassManager()  # All categories 
+    deleted_objects = DeletedClassManager() # Only soft-deleted categories
+    active_objects = ActiveClassManager() # Active categories (not deleted)
+
+    def __str__(self):
+        return self.name
+    
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        products = Products.objects.filter(category_id = self.id)
+        for product in products:
+            product.delete()
+        self.save()
+
+    def hard_delete(self, *args, **kwargs):
+        # Call the real delete method to permanently delete the object
+        products = Products.objects.filter(category_id=self.id)
+        for product in products:
+            product.hard_delete() 
+
+        super(Products, self).delete(*args, **kwargs)
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
 
 class Products(models.Model):
     name = models.CharField(max_length=255)
@@ -8,5 +55,29 @@ class Products(models.Model):
     stock = models.IntegerField()
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
+    # Soft delete field
+    is_deleted = models.BooleanField(default=False)
+
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='products')
+
+    objects = AllClassManager() # All products
+    deleted_objects = DeletedClassManager() # Only soft-deleted products
+    active_objects = ActiveClassManager() # Active products (not deleted)
+
     def __str__(self):
         return self.name
+    
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self, *args, **kwargs):
+        # Call the real delete method to permanently delete the object
+        super(Products, self).delete(*args, **kwargs)
+
+    def restore(self):
+        category = Categories.objects.get(id=self.category_id)
+        if not category.is_deleted:
+            self.is_deleted = False
+            self.save()
+        
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Products
+from .models import Categories, Products
 
 # Create your views here.
 def home(request):
@@ -16,19 +16,31 @@ def adminPanel(request):
 
 def adminProductProfile(request, id):
     product = get_object_or_404(Products, id=id)
-    return render(request, "productAdminProfile.html", { "product": product })
+    categories = Categories.objects.all()
+    return render(request, "productAdminProfile.html", { "product": product, "categories": categories })
 
 def adminProductEdit(request, id):
     product = get_object_or_404(Products, id=id)
+    categories = Categories.objects.all()
 
     if request.method == "POST":
         product = Products.objects.get(id=id)
         product.name = request.POST["name"]
         product.price = request.POST["price"]
         product.stock = request.POST["stock"]
+
+        if request.FILES.get('image'):
+            product.image = request.FILES['image']
+
+        category = Categories.objects.get(id = request.POST["category"])
+        if not category.is_deleted:
+            product.category = Categories.active_objects.get(id = request.POST["category"])
+        else:
+            return HttpResponse("Category does not exist")
+
         product.save()
 
-        return redirect('adminPanel')
+        return render(request, "productAdminProfile.html", { "product": product, "categories": categories })
     
     products = Products.objects.all()
     return render(request, "adminPanel.html", {"products": products})
@@ -38,3 +50,84 @@ def adminProductDelete(request, id):
 
     Products.objects.get(id=id).delete()
     return redirect('adminPanel')
+
+def adminProductRestore(request, id):
+    get_object_or_404(Products, id=id)
+
+    Products.deleted_objects.get(id=id).restore()
+    return redirect('adminPanel')
+
+def productAdminNewProductPage(request):
+    categories = Categories.objects.all()
+    return render(request, "productAdminNew.html", { "categories": categories })
+
+def adminProductCreate(request):
+    categories = Categories.objects.all()
+    if request.method == "POST":
+        product = Products()
+        product.name = request.POST["name"]
+        product.price = request.POST["price"]
+        product.stock = request.POST["stock"]
+
+        if request.FILES.get('image'):
+            product.image = request.FILES['image']
+
+        category = Categories.objects.get(id = request.POST["category"])
+        if not category.is_deleted:
+            product.category = Categories.active_objects.get(id = request.POST["category"])
+        else:
+            return HttpResponse("Category is deleted")
+
+        product.save()
+
+        return render(request, "productAdminProfile.html", { "product": product, "categories": categories })
+    
+    products = Products.objects.all()
+    return render(request, "adminPanel.html", {"products": products})
+
+def adminPanelCategories(request):
+    categories = Categories.objects.all()
+    return render(request, "adminPanelCategories.html", { "categories": categories })
+
+def categoriesAdminNewCategoryPage(request):
+    return render(request, "categoryAdminNew.html")
+
+def adminCategoryCreate(request):
+    if request.method == "POST":
+        category = Categories()
+        category.name = request.POST["name"]
+        category.save()
+
+        return redirect('adminPanelCategories')
+    
+    categories = Categories.objects.all()
+    return render(request, "adminPanelCategories.html", { "categories": categories })
+
+def adminCategoryProfile(request, id):
+    category = get_object_or_404(Categories, id=id)
+    return render(request, "categoryAdminProfile.html", { "category": category })
+
+def adminCategoryEdit(request, id):
+    category = get_object_or_404(Categories, id=id)
+
+    if request.method == "POST":
+        category = Categories.objects.get(id=id)
+        category.name = request.POST["name"]
+        category.save()
+
+        return render(request, "categoryAdminProfile.html", { "category": category })
+    
+    categories = Categories.objects.all()
+    return render(request, "adminPanelCategories.html", {"categories": categories})
+
+def adminCategoryDelete(request, id):
+    get_object_or_404(Categories, id=id)
+
+    Categories.objects.get(id=id).delete()
+    return redirect('adminPanelCategories')
+
+def adminCategoryRestore(request, id):
+    get_object_or_404(Categories, id=id)
+
+    Categories.deleted_objects.get(id=id).restore()
+    return redirect('adminPanelCategories')
