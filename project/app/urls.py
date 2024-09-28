a5d0bc751209101433409e358dc7e3b25cfc6202
Manuel <manuel_jose_2002@hotmail.com>
2024-09-28 18:35:00 +0100

    responsive Home

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
     # AUTH
     path('register/', views.register, name='register'),
     # Admin Panel - Dashboard
--- project/app/views.py
+++ project/app/views.py
@@ -70,10 +70,6 @@ def searchProductsForm(request, haveCategory):
 def home(request):
     return render(request, "home.html")
 
-def products(request):
-    products = Products.objects.all()
-    return render(request, "products.html", { "products": products })
-
 @login_required
 def adminPanel(request):
     nProds = Products.objects.count()
